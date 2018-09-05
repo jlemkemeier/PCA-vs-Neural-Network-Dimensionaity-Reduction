e59384f9eba89cc5dfc734a2bf1be095a95c0524
jlemkemeier <jlemkemeier@gmail.com>
2018-09-05 16:04:43 -0400

    Switching out the NN file for the new one

--- NN_Dimension_Reduction.py
+++ NN_Dimension_Reduction.py
@@ -1,9 +1,4 @@
 
-# coding: utf-8
-
-# In[ ]:
-
-
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
@@ -15,12 +10,8 @@ from mpl_toolkits.mplot3d import Axes3D
 import soundfile as sf
 
 
-# In[ ]:
-
-
 #import songs, and divide by their vector norm
 for x in range(1, 101):
-    #print(x)
     sig, samplerate = sf.read('/Users/jlemkemeier/Desktop/Cuts/' + str(x) +'c.wav')
     sig = np.array(sig)
 
@@ -30,30 +21,20 @@ for x in range(1, 101):
             sig = np.sum(sig, axis=1)/2
     except:
         print("not 2")
-    
+
     #dived each value in the audio clip by the vector norm
     for z in range(len(sig)):
         if z == 0:
             newsig = np.array([sig[z]/LA.norm(sig, np.inf)])
         else:
             newsig = np.hstack((newsig, [sig[z]/LA.norm(sig, np.inf)]))
-    
+
     if x == 1:
         total = np.array(newsig)
     else:
         total = np.vstack((total, newsig))
-
 print(len(total))
-
-
-# In[ ]:
-
-
 X = total
-
-
-# In[ ]:
-
 
 # Python optimisation variables
 epochs = 100000
@@ -62,18 +43,10 @@ batch_size = 25
 x_size = 2000
 y_size = 2000
 
-
-# In[ ]:
-
-
 # declare the training data placeholders
 x = tf.placeholder(tf.float32, [None, x_size], name='x')
 y = tf.placeholder(tf.float32, [None, y_size], name='y')
 points = np.array([])
-
-
-# In[ ]:
-
 
 # set up parameters
 W = []
@@ -86,37 +59,21 @@ W.append(tf.Variable(tf.random_normal([x_size, h_size[0]], stddev=0.03), name='W
 b.append(tf.Variable(tf.random_normal([h_size[0]]), name='b1'))
 layer.append(tf.nn.relu(tf.add(tf.matmul(x, W[0]), b[0])))
 
-
 for i in range(1,len(h_size)):
-    print(int(len(h_size)/2))
     W.append(tf.Variable(tf.random_normal([h_size[i-1], h_size[i]], stddev=0.03), name='W2'))
     b.append(tf.Variable(tf.random_normal([h_size[i]]), name='b2'))
     layer.append(tf.nn.relu(tf.matmul(layer[i-1], W[i])+ b[i]))
-    
     if(i == int(len(h_size)/2)):
         points = layer[-1]
 
 W.append(tf.Variable(tf.random_normal([h_size[-1], y_size], stddev=0.03), name='W3'))
 b.append(tf.Variable(tf.random_normal([y_size]), name='b3'))
 
-
-
-# In[ ]:
-
-
-# now calculate the hidden layer output - in this case, let's use a softmax
-# activated
-# output layer
 y_ = tf.nn.softmax(tf.add(tf.matmul(layer[-1], W[-1]), b[-1]))
-
 y_clipped = tf.clip_by_value(y_, 1e-10, 0.9999999)
 cross_entropy = -tf.reduce_mean(tf.reduce_sum(y * tf.log(y_clipped)
                          + (1 - y) * tf.log(1 - y_clipped), axis=1))
 mean_squared_error = tf.reduce_mean(tf.reduce_sum(tf.squared_difference(y_, y), axis=1))
-
-
-# In[ ]:
-
 
 # add an optimiser
 optimiser = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(mean_squared_error)
@@ -128,16 +85,7 @@ init_op = tf.global_variables_initializer()
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-
-# In[ ]:
-
-
-print(points)
-finalpoints = np.array([[0, 0]])
-
-
-# In[ ]:
-
+finalpoints = np.array([[0, 0, 0]])
 
 # start the session
 with tf.Session() as sess:
@@ -151,35 +99,26 @@ with tf.Session() as sess:
             batch_x = X[iteration:iteration+batch_size]
             batch_y = X[iteration:iteration+batch_size]
             iteration = iteration+batch_size
-            _, c = sess.run([optimiser, mean_squared_error], 
+            _, c = sess.run([optimiser, mean_squared_error],
                          feed_dict={x: batch_x, y: batch_y, learning_rate: .00001})
             avg_cost += c / total_batch
         if (epoch%100 == 0):
             print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost))
-    
+
     for z in range(1,100):
        finalpoints = np.vstack((finalpoints, points.eval(feed_dict={x: [X[z]]})))
     print(sess.run(accuracy, feed_dict={x: X, y: X}))
 print(finalpoints)
 
 
-# In[ ]:
-
-
 N = 50
-x1 = finalpoints[0:50,0]
+x1 = finalpoints[1:50,0]
 print(x1)
 x2 = finalpoints[50:100,0]
-y1 = finalpoints[0:50,1]
+y1 = finalpoints[1:50,1]
 y2 = finalpoints[50:100,1]
-z1 = finalpoints[0:50,2]
+z1 = finalpoints[1:50,2]
 z2 = finalpoints[50:100,2]
-
-
-
-
-# In[ ]:
-
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -187,16 +126,4 @@ ax.scatter(x1, y1, z1, c='blue', alpha=.5)
 ax.scatter(x2, y2, z2, c='red', alpha=.5)
 plt.show()
 
-
-
-# In[ ]:
-
-
 fig.savefig("3D2norm10000e10lr.pdf", bbox_inches='tight')
-
-
-# In[ ]:
-
-
-
-
